0dc87827454cfa9bf81ef91d0aab167f871e4754
root <root@rhel-ose-3-5-demo.stack.maskedadmins.com>
2017-06-27 22:03:59 -0400

    updating

--- buildandpush.sh
+++ buildandpush.sh
@@ -5,8 +5,9 @@ ose_pass="redhat123"
 container="cpuminer-multipool"
 docker build --no-cache -t $container .
 oc login -u $ose_user -p $ose_pass
-id=$(docker images|grep ^$container|awk '{print $3}')
+oc project default
 server=$(oc get svc|grep docker-reg| awk '{print $2}')
+id=$(docker images|grep ^${container}|awk '{print $3}')
 token=$(oc whoami -t)
 docker login -u $ose_user -p $token ${server}:5000
 docker tag $container ${server}:5000/${project}/${container}
--- etcd-container/rebuild.sh
+++ etcd-container/rebuild.sh
@@ -3,10 +3,11 @@ project="redhat-lab"
 ose_user="admin"
 ose_pass="redhat123"
 container="etcd-container"
+oc login default
+server=$(oc get svc|grep docker-reg| awk '{print $2}')
 docker build --no-cache -t $container .
 oc login -u $ose_user -p $ose_pass
 id=$(docker images|grep ^$container|awk '{print $3}')
-server=$(oc get svc|grep docker-reg| awk '{print $2}')
 token=$(oc whoami -t)
 docker login -u $ose_user -p $token ${server}:5000
 docker tag $container ${server}:5000/${project}/${container}
